1783adc5b8addffb811ab1d956d850368bbeb3ec
Ednilio <129685239+Ednilio@users.noreply.github.com>
2026-05-25 17:57:23 +0200

    Add files via upload

---
+++ Imbalance RQ/02 Oversampled testing/test_bal_yolov26m.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+#SBATCH --job-name=yolov26m_bal_eval_test
+#SBATCH --partition=GPU
+#SBATCH --gres=gpu:1
+#SBATCH --time=03:00:00
+#SBATCH --output=slurm_%j.out
+#SBATCH --error=slurm_%j.err
+
+echo "Starting YOLOv26m oversampled TEST evaluation"
+
+# Conda activation
+if [ -f "/usr/local/anaconda3/etc/profile.d/conda.sh" ]; then
+    . "/usr/local/anaconda3/etc/profile.d/conda.sh"
+else
+    export PATH="/usr/local/anaconda3/bin:$PATH"
+fi
+
+conda activate YOLOenv
+
+cd /home/u928808/Thesis || exit 1
+
+yolo detect val \
+    model=runs/detect/experiments/oversampled_yolov26_sh17/yolov26m/weights/best.pt \
+    data=data_for_yolo/sh17_over.yaml \
+    split=test \
+    imgsz=640 \
+    batch=32 \
+    device=0 \
+    project=experiments/oversampled_eval_yolov26_sh17 \
+    name=yolov26m_test
+
+echo "Evaluation finished"
